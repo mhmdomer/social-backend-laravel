6cafbda64ba6978136d9c125127295c1eb41059e
m7md3omer <mohammedomer789@gmail.com>
2020-06-23 01:09:05 +0200

    added a proper error message for unauthenticated users

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function() {
-    return 'test';
+    return response(['message' => 'Please login first'], 403);
 })->name('login');
 Route::post('/posts', 'Api\PostsController@store')->name('store');
 Route::delete('/posts/{post}', 'Api\PostsController@destroy')->name('destroy');
