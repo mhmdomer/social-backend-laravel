65513f27ddddf0f51f87059b3d56c0118437f230
m7md3omer <mohammedomer789@gmail.com>
2020-09-04 07:13:59 +0200

    fixed follower and following relations

--- app/User.php
+++ app/User.php
@@ -52,12 +52,12 @@ class User extends Authenticatable
 
     public function followers()
     {
-        return $this->belongsToMany(User::class, 'followers', 'following_id', 'follower_id');
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'following_id');
     }
 
     public function following()
     {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'following_id');
+        return $this->belongsToMany(User::class, 'followers', 'following_id', 'follower_id');
     }
 
     public function follow(User $user)
